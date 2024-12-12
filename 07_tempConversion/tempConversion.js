2b08cd2683a0c33652caee47117eb2e88e6963e5
Big-Plato <luisoctavius.sc@gmail.com>
2024-12-12 20:20:12 -0300

    Make Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = parseFloat((temp - 32) * 5/9);
+  let tempRounded = Math.round(temp * 10) / 10;
+  return tempRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = parseFloat((temp * 9/5 + 32));
+  let tempRounded = Math.round(temp * 10) / 10;
+  return tempRounded;
 };
 
 // Do not edit below this line
