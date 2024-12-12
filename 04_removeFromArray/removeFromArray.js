a3d1a5cfa070dd55518920f0ea471c68fec4d29b
Big-Plato <luisoctavius.sc@gmail.com>
2024-12-12 18:52:58 -0300

    Make Exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
 const removeFromArray = function() {
+   const args = Array.from(arguments);
+   let arr = [];
+   for (let i = 0; i > args.length - 1; i++) {
+    if (args.indexOf(arr[i]) == -1) {
+        arr.push(arr[i]);
+    }
+   }
+   return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
 
+    if (!Number.isInteger(n1) || ((!Number.isInteger(n2)))) {
+        return 'ERROR'; 
+    } 
+    if (n1 < 0 || n2 < 0) {
+        return 'ERROR';
+    }
+
+    if (n1 > n2) { //n1 = 123 n2 = 1
+        const value = n1;
+        n1 = n2;
+        n2 = value;
+    } 
+    
+    let total = 0;
+
+    for (let i = n1; i <= n2; i++) {
+        total += i;
+    }
+
+    return total;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
