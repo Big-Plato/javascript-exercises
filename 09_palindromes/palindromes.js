4fbc8edf903cb713c7b5d1c5bf8b5b70fc57bff8
Big-Plato <luisoctavius.sc@gmail.com>
2024-12-13 18:34:34 -0300

    Make Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz01234566789';
+    
+    let arrange = word.toLowerCase().split("").filter((character) => alphanumerical.includes(character)).join("");
 
-};
+    let reversed = arrange.split("").reverse().join("");
+
+
+    console.log(arrange);
+
+    if (reversed === arrange) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
